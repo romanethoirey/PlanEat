09d72c65456876123052bd1803c2c22269752e7a
romane <rthoirey@hotmail.fr>
2019-11-13 17:34:29 +0100

    update database

--- BDD/bdd.sql
+++ BDD/bdd.sql
@@ -15,11 +15,9 @@ USE planeat;
 --
 -- Structure de la table `users`
 --
-
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(64) NOT NULL AUTO_INCREMENT,
   `user_login` varchar(32) NOT NULL,
   `user_password` blob NOT NULL,
   `user_mail` varchar(24) DEFAULT NULL,
@@ -30,6 +28,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `user_login` (`user_login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
 -- --------------------------------------------------------
 
 --
@@ -37,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 DROP TABLE IF EXISTS `ingredients`;
 CREATE TABLE IF NOT EXISTS `ingredients` (
-  `ingredient_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ingredient_id` int(64) NOT NULL AUTO_INCREMENT,
   `ingredient_nom` varchar(16) DEFAULT NULL,
   `ingredient_type` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`ingredient_id`)
@@ -47,44 +46,43 @@ CREATE TABLE IF NOT EXISTS `ingredients` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `receipes`
+-- Structure de la table `recettes`
 --
-DROP TABLE IF EXISTS `receipes`;
-CREATE TABLE IF NOT EXISTS `receipes` (
-  `receipe_id` int(11) NOT NULL AUTO_INCREMENT,
-  `receipe_nom` varchar(16) DEFAULT NULL,
-  `receipe_description` varchar(64) DEFAULT NULL,
-  `receipe_nb_person` int(64) DEFAULT NULL,
-  `receipe_prep_time` int(64) DEFAULT NULL,
-  `receipe_step` int(64) DEFAULT NULL,
-  PRIMARY KEY (`receipe_id`)
+DROP TABLE IF EXISTS `recettes`;
+CREATE TABLE IF NOT EXISTS `recettes` (
+  `recette_id` int(64) NOT NULL AUTO_INCREMENT,
+  `recette_nom` varchar(16) DEFAULT NULL,
+  `recette_description` varchar(128) DEFAULT NULL,
+  `recette_nb_personnes` int(64) DEFAULT NULL,
+  `recette_temps_prep` int(64) DEFAULT NULL,
+  PRIMARY KEY (`recette_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users_receipes`
+-- Structure de la table `users_recettes`
 --
-DROP TABLE IF EXISTS `users_receipes`;
-CREATE TABLE IF NOT EXISTS `users_receipes` (
-  `receipe_id` int(11) NOT NULL,
-  `users_id` int(11) NOT NULL,
-  PRIMARY KEY (`receipe_id`, `users_id`)
+DROP TABLE IF EXISTS `users_recettes`;
+CREATE TABLE IF NOT EXISTS `users_recettes` (
+  `recette_id` int(64) NOT NULL,
+  `users_id` int(64) NOT NULL,
+  PRIMARY KEY (`recette_id`, `users_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `ingredients_receipe`
+-- Structure de la table `ingredients_recette`
 --
-DROP TABLE IF EXISTS `ingredients_receipe`;
-CREATE TABLE IF NOT EXISTS `ingredients_receipe` (
-  `receipe_id` int(11) NOT NULL,
-  `ingredient_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  PRIMARY KEY (`receipe_id`, `ingredient_id`)
+DROP TABLE IF EXISTS `ingredients_recette`;
+CREATE TABLE IF NOT EXISTS `ingredients_recette` (
+  `recette_id` int(64) NOT NULL,
+  `ingredient_id` int(64) NOT NULL,
+  `quantity` int(64) NOT NULL,
+  PRIMARY KEY (`recette_id`, `ingredient_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -95,40 +93,24 @@ CREATE TABLE IF NOT EXISTS `ingredients_receipe` (
 --
 DROP TABLE IF EXISTS `planning`;
 CREATE TABLE IF NOT EXISTS `planning` (
-  `receipe_id` int(11) NOT NULL,
-  `date_receipe` date NOT NULL,
-  `planning_user_id` date NOT NULL,
-  PRIMARY KEY (`receipe_id`, `date_receipe`)
+  `planning_recette_id` int(64) NOT NULL,
+  `planning_date_recette` date NOT NULL,
+  `planning_user_id` int(64) NOT NULL,
+  PRIMARY KEY (`planning_recette_id`, `planning_date_recette`, `planning_user_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `shop_list`
+-- Structure de la table `produits`
 --
-DROP TABLE IF EXISTS `shop_list`;
-CREATE TABLE IF NOT EXISTS `shop_list` (
-  `shop_list_id` int(11) NOT NULL,
-  `date_shop_list` date NOT NULL,
-  `amount` int(11) DEFAULT 0,
-  `shop_list_user_id` date NOT NULL,
-  PRIMARY KEY (`shop_list_id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- --------------------------------------------------------
-
---
--- Structure de la table `produit`
---
-DROP TABLE IF EXISTS `produit`;
-CREATE TABLE IF NOT EXISTS `produit` (
-  `produit_id` int(11) NOT NULL,
-  `produit_nom` int(11) NOT NULL,
-  `produit_amount` int(11) DEFAULT 0,
-  `produit_shop` varchar NOT NULL,
-  `produit_quantity` int(11) NOT NULL,
+DROP TABLE IF EXISTS `produits`;
+CREATE TABLE IF NOT EXISTS `produits` (
+  `produit_id` int(64) NOT NULL,
+  `produit_nom` int(64) NOT NULL,
+  `produit_quantité` int(64) NOT NULL,
+  `produit_ingredient_id` int(64) NOT NULL,
   PRIMARY KEY (`produit_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -136,24 +118,66 @@ CREATE TABLE IF NOT EXISTS `produit` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `produit_shop_list`
+-- Structure de la table `liste_courses`
 --
-DROP TABLE IF EXISTS `produit_shop_list`;
-CREATE TABLE IF NOT EXISTS `produit_shop_list` (
-  `produit_id` int(11) NOT NULL,
-  `shop_list_id` int(11) NOT NULL,
-  PRIMARY KEY (`produit_id`, `shop_list_id`)
+DROP TABLE IF EXISTS `liste_courses`;
+CREATE TABLE IF NOT EXISTS `liste_courses` (
+  `liste_courses_id` int(64) NOT NULL,
+  `date_liste_courses` date NOT NULL,
+  `amount` int(64) DEFAULT 0,
+  `liste_courses_user_id` int(64) NOT NULL,
+  PRIMARY KEY (`liste_courses_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `produit_ingredient`
+-- Structure de la table `produit_liste_courses`
 --
-DROP TABLE IF EXISTS `produit_shop_list`;
-CREATE TABLE IF NOT EXISTS `produit_shop_list` (
-  `produit_id` int(11) NOT NULL,
-  `ingredient_id` int(11) NOT NULL,
-  PRIMARY KEY (`produit_id`, `ingredient_id`)
+DROP TABLE IF EXISTS `produit_liste_courses`;
+CREATE TABLE IF NOT EXISTS `produit_liste_courses` (
+  `produit_id` int(64) NOT NULL,
+  `liste_courses_id` int(64) NOT NULL,
+  PRIMARY KEY (`produit_id`, `liste_courses_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `magasins`
+--
+DROP TABLE IF EXISTS `magasins`;
+CREATE TABLE IF NOT EXISTS `magasins` (
+  `magasin_id` int(64) NOT NULL AUTO_INCREMENT,
+  `magasin_nom` varchar(64) NOT NULL,
+  PRIMARY KEY (`magasin_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE IF NOT EXISTS `articles` (
+  `magasin_id` int(64) NOT NULL,
+  `produit_id` int(64) NOT NULL,
+  PRIMARY KEY (`produit_id`, `magasin_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `panier`
+--
+DROP TABLE IF EXISTS `panier`;
+CREATE TABLE IF NOT EXISTS `panier` (
+  `liste_courses_id` int(64) NOT NULL,
+  `recette_id` int(64) NOT NULL,
+  PRIMARY KEY (`liste_courses_id`, `recette_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
